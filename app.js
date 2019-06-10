d283ff6203cedd8a6b5271565541c6243d3e1361
Emanuel4168 <emanuel.l4168@gmail.com>
2019-06-10 11:51:58 -0600

    Creada función saveTask

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert('this shit works')
+document.getElementById('taskForm').addEventListener('submit',saveTask)
+
+function saveTask(evt){
+    evt.preventDefault()
+    const title = document.getElementById('txtCardTitle').value
+    const body = document.getElementById('txtCardBody').value
+
+    const task ={
+        title,
+        body
+    }
+
+    if(localStorage.getItem('tasks') == null){
+        let tasks = [task]
+        //tasks.push(task)
+        localStorage.setItem('tasks',JSON.stringify(tasks))
+        return
+    }
+    let tasks = JSON.parse(localStorage.getItem('tasks'))
+    tasks.push(task)
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+}
